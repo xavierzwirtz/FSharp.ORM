1927f0ca3089cb8d8ed3404249e578908d741da4
voiceofwisdom <me@xavierzwirtz.com>
2015-11-03 14:56:12 -0600

    Updated AssemblyInfo.fs

--- src/FSharp.ORM.Core/AssemblyInfo.fs
+++ src/FSharp.ORM.Core/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.ORM.Core")>]
 [<assembly: AssemblyProductAttribute("FSharp.ORM")>]
 [<assembly: AssemblyDescriptionAttribute(".NET ORM, primarily for F#")>]
-[<assembly: AssemblyVersionAttribute("1.8.0.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.8.0.0")>]
+[<assembly: AssemblyVersionAttribute("1.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.8.0.0"
+    let [<Literal>] Version = "1.0.0"
